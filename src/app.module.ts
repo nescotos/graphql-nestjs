8da9b2e2bcffb51a3c3f05f6fdba9298da43f2cd
Nestor Escoto <nescoto@Nestors-MacBook-Pro.local>
2020-05-04 20:40:33 -0600

    Initial Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'graphql',
+      username: 'postgres',
+      password: 'toor'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
